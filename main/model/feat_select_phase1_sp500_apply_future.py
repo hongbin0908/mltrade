54028dd5fecdb6f4f531de37f7e8f5e5eed1d9e7
hongbin <hongbin0908@126.com>
2016-09-06 22:06:52 +0800

    xxxx

--- main/model/feat_select_phase1_sp500_apply_future.py
+++ main/model/feat_select_phase1_sp500_apply_future.py
@@ -13,6 +13,18 @@ from main.model import feat_select
 
 dataroot = os.path.join(root, "data", "feat_select")
 
+def work(df, f):
+    for i in range(10):
+        frm = 50  * i
+        to  = frm + 50
+        setname = "sp500R%dT%d" % (frm, to)
+        taname = "base1"
+        (phase1, phase2, phase3) = \
+            feat_select.split_dates(feat_select.load_feat(taname, setname))
+        df2 = feat_select.apply(df,phase2, "label5", "_p2")
+        df2 = feat_select.apply(df2,phase3, "label5", "_p3")
+        feat_select.ana2(df2, f, setname)
+
 if __name__ == '__main__':
     fout = os.path.join(dataroot,
                         "feat_select_phase1_sp500_apply_future.ana")
@@ -28,16 +40,5 @@ if __name__ == '__main__':
     work(df, f)
     f.close()
 
-def work(df, f):
-    for i in range(10):
-        frm = 50  * i
-        to  = frm + 50
-        setname = "sp500R%dT%d" % (frm, to)
-        taname = "base1"
-        (phase1, phase2, phase3) = \
-            feat_select.split_dates(feat_select.load_feat(taname, setname))
-        df2 = feat_select.apply(df,phase2, "label5", "_p2")
-        df2 = feat_select.apply(df2,phase3, "label5", "_p3")
-        feat_select.ana2(df2, f, setname)
 
 
