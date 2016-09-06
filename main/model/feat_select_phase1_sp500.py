a3313b1fcf112bbdb5a3b4e3f777fc803747059e
hongbin <hongbin0908@126.com>
2016-09-06 18:07:58 +0800

    xxxx

--- main/model/feat_select_phase1_sp500.py
+++ main/model/feat_select_phase1_sp500.py
@@ -23,6 +23,10 @@ if __name__ == '__main__':
     feat_select.ana_fmetas(df, "base1", "sp500", f)
 
     abs_direct_p_set = set(df[df.direct == 1].name.unique())
+    abs_direct_n_set = set(df[df.direct == -1].name.unique())
+
+    orig_direct_p_set = abs_direct_p_set.copy()
+    orig_direct_n_set = abs_direct_n_set.copy()
     print len(abs_direct_p_set)
     print >>f, "="*8
     for i in range(10):
@@ -41,13 +45,40 @@ if __name__ == '__main__':
 
             df2.to_pickle(filename)
         df2 = pd.read_pickle(filename)
-        cur_set = set(df2[df2.direct_p1 == 1].name.unique())
-        abs_direct_p_set = abs_direct_p_set.intersection(cur_set)
-        print len(abs_direct_p_set)
+        cur_p_set = set(df2[df2.direct_p1 == 1].name.unique())
+        cur_n_set = set(df2[df2.direct_p1 == -1].name.unique())
+        abs_direct_p_set = abs_direct_p_set.intersection(cur_p_set)
+        abs_direct_n_set = abs_direct_n_set.intersection(cur_n_set)
+
+    print >> f, "## stable feats on postive direct"
     for name in abs_direct_p_set:
         idx = 0
         for i, each in df[df.name == name].iterrows():
-            print each["fname"], each["start"],each["end"]
+            print >>f, "|%s|%.4f|%.4f|" % (each["fname"],each["start"],each["end"])
+            assert idx < 1
+            idx += 1
+
+    print >> f, "## UNstable feats on postive direct"
+    for name in orig_direct_p_set - abs_direct_p_set:
+        idx = 0
+        for i, each in df[df.name == name].iterrows():
+            print >>f, "|%s|%.4f|%.4f|" % (each["fname"],each["start"],each["end"])
+            assert idx < 1
+            idx += 1
+
+    print >> f, "## stable feats on negtive direct"
+    for name in abs_direct_n_set:
+        idx = 0
+        for i, each in df[df.name == name].iterrows():
+            print >>f, "|%s|%.4f|%.4f|" % (each["fname"],each["start"],each["end"])
+            assert idx < 1
+            idx += 1
+
+    print >> f, "## unstable feats on negtive direct"
+    for name in orig_direct_n_set - abs_direct_n_set:
+        idx = 0
+        for i, each in df[df.name == name].iterrows():
+            print >>f, "|%s|%.4f|%.4f|" % (each["fname"],each["start"],each["end"])
             assert idx < 1
             idx += 1
 
