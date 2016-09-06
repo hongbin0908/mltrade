7d8a8609bd4b1f3747dfe46f7d56c57441159147
hongbin <hongbin0908@126.com>
2016-09-06 22:42:37 +0800

    xxxx

--- main/model/feat_select_phase1_sp500_apply_future.py
+++ main/model/feat_select_phase1_sp500_apply_future.py
@@ -25,6 +25,15 @@ def work(df, f):
         df2 = feat_select.apply(df2,phase3, "label5", "_p3")
         feat_select.ana2(df2, f, setname)
 
+def work2(df, f):
+    setname = "sp500"
+    taname = "base1"
+    (phase1, phase2, phase3) = \
+        feat_select.split_dates(feat_select.load_feat(taname, setname))
+    df2 = feat_select.apply(df,phase2, "label5", "_p2")
+    df2 = feat_select.apply(df2,phase3, "label5", "_p3")
+    feat_select.ana2(df2, f, setname)
+
 if __name__ == '__main__':
     fout = os.path.join(dataroot,
                         "feat_select_phase1_sp500_apply_future.ana")
@@ -34,12 +43,12 @@ if __name__ == '__main__':
                               "phase1_dump",
                               "sp500_base1_stable.pkl"))
 
-    work(df, f)
+    work2(df, f)
     print >> f, "## sp500_base1.pkl"
     df = pd.read_pickle(os.path.join(dataroot,
                               "phase1_dump",
                               "sp500_base1.pkl"))
-    work(df, f)
+    work2(df, f)
     f.close()
 
 
