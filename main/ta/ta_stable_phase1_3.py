0202acaa8a06861bdb378f70c3729f3c00ff23e5
hongbin <hongbin0908@126.com>
2016-09-09 12:47:46 +0800

    xxxx

--- main/ta/ta_stable_phase1_3.py
+++ main/ta/ta_stable_phase1_3.py
@@ -23,7 +23,7 @@ import main.ta.ta_cdl as cdl
 def merge(df, depth):
     dfStable = pd.read_pickle(os.path.join(dataroot,
                               "phase1_dump",
-                              "sp500_base1_%d_stable.pkl" % depth))
+                              "sp500_base1_%s_%d_stable.pkl" % ("all",depth))
     dfStable = dfStable[dfStable.direct != 0]
     tobe = []
     for i, each in  dfStable.iterrows():
